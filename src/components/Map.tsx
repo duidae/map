ce198cc5056194b766535e70f81e517584f9f1ce
duidae <duidae@gmail.com>
2019-05-09 13:31:47 +0800

    Refine the code

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -4,21 +4,14 @@ import 'leaflet/dist/leaflet.css';
 import "./Map.css"
 
 export class RenewalMap extends React.Component {
-  private static TAIPEI = {lat: 25.038357847174, lng: 121.54770626982};
-  private static ZOOM_INITIAL = 13.4;
+  private static readonly TAIPEI = {lat: 25.038357847174, lng: 121.54770626982};
+  private static readonly ZOOM_INITIAL = 13.4;
 
   render() {
     return (
       <LeafletMap
         center={RenewalMap.TAIPEI}
         zoom={RenewalMap.ZOOM_INITIAL}
-        attributionControl={true}
-        zoomControl={true}
-        doubleClickZoom={true}
-        scrollWheelZoom={true}
-        dragging={true}
-        animate={true}
-        easeLinearity={0.35}
       >
         <TileLayer
           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
